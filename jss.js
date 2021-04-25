202d68d4d5a2bb2264ab20e28bc8971ef1e471bd
Dovydas-glitch <80913418+Dovydas-glitch@users.noreply.github.com>
2021-04-25 19:33:40 +0300

    uzlinko pc

--- jss.js
+++ jss.js
@@ -1,50 +1,23 @@
-// Mobile menu
-document.getElementById('mobile-menu-trigger').addEventListener('click', function() {
+document.getElementById('mobileBurger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.getElementById('navigation').classList.toggle('active');
-});
-
-// TODO: paspaudus nuorodą mobile menu, turėtų susiskleisti
-
-
-// Tabs section
-let tabNavigationBlocks = document.querySelectorAll('.tab-section nav ul li');
-let tabContentBlocks = document.querySelectorAll('.tab-section .tab-content');
-
-function removeActiveNavigation() {
-  for (let tab of tabNavigationBlocks) {
-    tab.classList.remove('active');
-  }
-}
-
-function removeActiveTabs() {
-  for (let tab of tabContentBlocks) {
-    tab.classList.remove('active');
-  }
-}
-
-for (let tab of tabNavigationBlocks) {
-  tab.addEventListener('click', function() {
-    removeActiveNavigation();
-    removeActiveTabs();
-
-    this.classList.add('active');
-
-    let index = this.dataset.slide;
-
-    document.querySelector('.tab-section .tab-content[data-slide="'+index+'"]').classList.add('active');
-    
-    // swiper.slideTo(index - 1);
-  })
-}
-
-
-// Reviews swiper
-const swiper = new Swiper('.reviews-swiper', {
-    slidesPerView: 1,
-    autoHeight: true,
-
-    breakpoints: {
+  });
+  
+  
+  
+  document.getElementById('menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('navigation').classList.remove('active');
+  });
+  
+  
+  
+  const swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      slidesPerView: 1,
+      loop: true,
+      autoHeight: true,
+      breakpoints: {
         // when window width is >= 320px
         600: {
           slidesPerView: 2,
@@ -54,18 +27,37 @@ const swiper = new Swiper('.reviews-swiper', {
           slidesPerView: 3,
         }
       },
-
-    // autoplay: {
-    //     delay: 5000
-    // },
-
-    // Optional parameters
-    // loop: true,
+     
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    });
+    
+  let tabNavigationBlocks = document.querySelectorAll('.tab_section nav ul li');
+  let tabContentBlocks = document.querySelectorAll('.tab_section .tab-content');
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-
-  });
+  function removeActiveNavigation() {
+    for (let tab of tabNavigationBlocks) {
+      tab.classList.remove('active');
+    }
+  }
+  
+  function removeActiveTabs() {
+    for (let tab of tabContentBlocks) {
+      tab.classList.remove('active');
+    }
+  }
+  
+  for (let tab of tabNavigationBlocks) {
+    tab.addEventListener('click', function() {
+      removeActiveNavigation();
+      removeActiveTabs();
+  
+      this.classList.add('active');
+  
+      let index = this.dataset.slide;
+  
+      document.querySelector('.tab_section .tab-content[data-slide="'+index+'"]').classList.add('active');
+    })
+  }
